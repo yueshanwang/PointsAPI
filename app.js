1420b791c06451397199c3f13128ddf105f06f46
yueshanwang <yueshan314@gmail.com>
2022-11-30 22:24:34 -0600

    Update app.js

--- app.js
+++ app.js
@@ -4,16 +4,13 @@ app.use(express.json());
 
 var port = process.env.PORT || 4000;
 
-// insert elements in order of timestamp
-//timestamp should sort normally
-var transactions = [
+//array of transactions, both adding and spending
+var transactions = []
 
-    {payer: "payer1", points: 300, timestamp:"2022-10-31T10:00:00Z"},
-    {payer: "payer2", points: 300, timestamp:"2022-10-31T10:00:00Z"},
-    {payer: "payer3", points: 10000, timestamp: "2022-10-31T11:00:00Z" },
-    {payer: "payer1", points: 1000, timestamp:"2022-10-31T10:00:00Z"}
-]
+//array of remaining points still spendable
+var remainingPoints = []
 
+//comparison function that compares based on timestamp
 function compare(a,b) {
 
     if (a.timestamp < b.timestamp){
@@ -25,38 +22,26 @@ function compare(a,b) {
     return 0;
 }
 
-//go through list of add transactions and minus payers' points from the spend points sum
-
 app.get('/', (req, res) => {
-
     res.send('Welcome to Yueshan\'s web service');
-
-});
-
-//view transactions list
-app.get('/api/transactions', (req, res) => {
-
-    res.send(transactions);
-
 });
 
 //add transaction route
 app.put("/api/add", (req, res) =>{
 
     if(!req.body){
-     res.status(400).send("no body in request");
+     res.status(400).send("No body in request");
      return;
     }
 
-    if (!req.body.payer || req.body.payer.length < 3){
-    
-        res.status(400).send("bad name");
+    if (!req.body.payer){
+        res.status(400).send("No payer name");
         return;
     }
 
     var timestampJSON = req.body.timestamp;
 
-    if(!timestampJSON){
+    if(!req.body.timestamp){
         timestampJSON = new Date().toJSON();
     }
 
@@ -66,30 +51,44 @@ app.put("/api/add", (req, res) =>{
     timestamp: timestampJSON
   };
 
+  const remaining = {
+      payer: req.body.payer,
+      points: req.body.points,
+      timestamp: timestampJSON
+  }
+
   transactions.push(transaction);
   transactions.sort(compare);
+  remainingPoints.push(remaining);
+  remainingPoints.sort(compare);
   res.send(transaction);
-
 });
-
 
 //spend transaction route
 app.post('/api/spend/:amount', (req, res) => {
 
+    if(req.params.amount<0){
+        res.status(400).send("Spend amount cannot be negative");
+        return;
+    }
+
     let total = req.params.amount;
-    const toRemove=[];
     const map = new Map();
-    for (var i = 0; i<transactions.length && total>0; i++){
-        var obj = transactions[i];
+    const toRemove = [];
+    //loop runs until end of remaining points array or total is 0
+    for (var i = 0; i<remainingPoints.length && total>0; i++){
+        var obj = remainingPoints[i];
 
         var toSpend = Math.min(total, obj.points);
-
         total = total-toSpend;
-
         obj.points = obj.points-toSpend;
 
-        if(!map.has(obj.payer)){
+        //if no points left, mark for removal
+        if(obj.points == 0){
+            toRemove.push(i);
+        }
 
+        if(!map.has(obj.payer)){
             map.set(obj.payer, 0-toSpend);
         }
         else{
@@ -97,9 +96,41 @@ app.post('/api/spend/:amount', (req, res) => {
         }
     }
 
-    const values = Array.from(map);
+    if(total > 0){
+         res.status(400).send("Not enough points");
+         return;
+    }
 
-    res.send(values);
+    //removing 0 point values from array,
+    //starting at end of array
+    for(var i = toRemove.length - 1; i >=0; i--){
+        remainingPoints.splice(toRemove[i],1);
+    }
+
+    //adding spend transactions to transaction list
+    for (let [key, value] of map){
+
+        const timestampJSON = new Date().toJSON();
+        const transaction = {
+            payer: key,
+            points: value,
+            timestamp: timestampJSON
+        };
+
+        transactions.push(transaction);
+        transactions.sort(compare);
+    }
+    var toReturn = [];
+    for (let [key, value] of map){
+
+        const json = {
+            payer: key,
+            points: value
+        }
+        toReturn.push(json)
+    }
+
+    res.send(toReturn);
 });
 
 
@@ -111,7 +142,6 @@ app.get('/api/balances', (req, res) => {
       var obj = transactions[i];
 
         if(!map.has(obj.payer)){
-
              map.set(obj.payer, obj.points);
          }
          else{
@@ -122,6 +152,15 @@ app.get('/api/balances', (req, res) => {
     const json = JSON.stringify(Object.fromEntries(map));
 
     res.send(json);
+});
+
+//utility routes to view transactions and remaining points array
+app.get('/api/transactions', (req, res) => {
+    res.send(transactions);
+});
+
+app.get('/api/remaining', (req, res) => {
+    res.send(remainingPoints);
 });
 
 app.listen(port, function () {
